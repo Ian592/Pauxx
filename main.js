32b8dc6d941b82fa44ebb120ecafcdf219655720
Ian592 <64840164+Ian592@users.noreply.github.com>
2020-05-05 06:08:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>  {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.portada');
+ScrollReveal().reveal('.Nuevas-noticias', { delay: 500});
+ScrollReveal().reveal('.cards-banner-one', { delay: 500});
+ScrollReveal().reveal('.cards-banner-two', { delay: 500});
